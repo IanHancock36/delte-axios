c984e3ebe697cc991a361c2fe5c984bc2c43f64b
Ian Hancock <northroadian@gmail.com>
2022-02-06 14:44:37 -0600

    source data from JSON coming through

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
-  useEffect(()=>{
+  useEffect(() => {
     axios.get('https://jsonplaceholder.typicode.com/posts')
-.then(response => console.log(response.data)).catch(error => console.log(error))
-  },[])
+      .then(response => console.log(response.data)).catch(error => console.log(error))
+  }, [])
   return (
     <div className="App">
       <h1>AXIOS GET POST DELETE</h1>
-   
     </div>
   );
 }
