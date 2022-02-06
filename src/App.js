f5b72e66791d6ab8f4e651d7fbd797f3390f58bf
Ian Hancock <northroadian@gmail.com>
2022-02-06 14:54:38 -0600

    map

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
+  const [data, setData]= useState([])
   useEffect(() => {
     axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then(response => console.log(response.data)).catch(error => console.log(error))
+     .then(response => {
+       console.log("get data", response.data)
+       setData(response.data)
+     }).catch(error => console.log(error))
   }, [])
   return (
     <div className="App">
       <h1>AXIOS GET POST DELETE</h1>
+      {data.map((title)=> (
+      
+             <div>{data.title}</div>
+      ))}
+      <div></div>
     </div>
   );
 }
