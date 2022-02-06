12f5c0ec0c4d491f386d23bf11e502d61fb4c0fc
Ian Hancock <northroadian@gmail.com>
2022-02-06 14:43:59 -0600

    useeffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  useEffect(()=>{
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+.then(response => console.log(response.data)).catch(error => console.log(error))
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>AXIOS GET POST DELETE</h1>
+   
     </div>
   );
 }
