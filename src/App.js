dcd8965b76de3ff03cf65dfdd136fa5d7a9dc9f8
Ian Hancock <northroadian@gmail.com>
2022-02-06 15:18:32 -0600

    post

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 function App() {
   const [data, setData]= useState([])
+  
   useEffect(() => {
     axios.get('https://jsonplaceholder.typicode.com/posts')
      .then(response => {
@@ -10,10 +11,11 @@ function App() {
        setData(response.data)
      }).catch(error => console.log(error))
   }, [])
+  // I like rendering the map then taking the variable name and applying it like below
   const arr = data.map((data , index )=> {
     return (
       <div>
-      <div>
+      <div style ={{border: '1px solid black'}}>
         {data.id}
       </div>
       <div>{data.title}</div>
@@ -24,7 +26,19 @@ function App() {
   return (
     <div className="App">
       <h1>AXIOS GET POST DELETE</h1>
-   
+      <form>
+        <div>
+        <div>
+      <h1>Id</h1>
+        <input/>
+        </div>
+        <div>
+      <h1>Title</h1>
+        <input/>
+        </div>
+        <button title="SEND POST"/>
+        </div>
+      </form>
       <div>{arr}</div>
     </div>
   );
